1e1b46329e63d3889cde9bd93a17bf1fe71a13ed
Wang Nan <pi3orama@163.com>
2020-06-08 01:00:07 +0800

    sync: Sync with Linux 5.7
    Signed-off-by: Wang Nan <pi3orama@163.com>

---
+++ sync_with_linux.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+function usage()
+{
+  echo "Usage:"
+  echo "$0 <path-to-linux-kernel>"
+  exit 1
+}
+
+if [ ! -d "$1" ];
+then
+  usage $0
+fi
+
+LINUX_SRC="$1"
+DST="."
+
+rm -rf ${DST}/fixdep
+cp -rv ${LINUX_SRC}/scripts/basic ${DST}/fixdep
+
+for f in Kbuild.include  Makefile.build  Makefile.clean  Makefile.host  Makefile.lib; do
+  cp -v ${LINUX_SRC}/scripts/$f ${DST}/kbuild/scripts/$f
+done
+
+rm -rf ${DST}/kconfig
+cp -rv ${LINUX_SRC}/scripts/kconfig ${DST}/kconfig
+cp -v ${LINUX_SRC}/scripts/unifdef.c ${DST}/unifdef/
